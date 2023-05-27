c9b5e6a89a2c7c98f1185eeeab90f9ddac6fab48
Rochabernardo <130403141+denidev0101@users.noreply.github.com>
2023-05-27 09:24:12 -0300

    Denilson

--- index.php
+++ index.php
@@ -1,3 +1,62 @@
+<?php 
+require ("dadosgerais.php");
+$type = $_GET["type"] ?? '';
+
+    if($type=="mansoes"){
+      $data=$geral["Mansões"];
+      $type="mansoes";
+    }else if($type=="quartos"){
+      $data=$geral["Quartos"];
+      $type="quartos";
+
+    }else if($type=="fazendas"){
+      $data=$geral["Fazendas"];
+      $type="fazendas";
+
+    }else{
+      $data=$geral;
+    }
+    
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Área cheap</title>
+  <link rel="stylesheet"  href="estilizar.css">
+</head>
+
+<body>
+  <header>
+      <a href="index.php"> <img src="ImgCategorias/logo.png" alt="Logo Área Cheap" id="logo"> </a>
+            <form id="btn">
+              <input type="search" name="busca" placeholder="Busca">
+              <p>&#x1F50D;</p>
+            </form>
+          <a href="cadastro.php">Cadastro</a>
+          <a href="login.php">Login</a>
+          <a href="saibamais.php">Saiba mais</a>
+
+  </header>
+    <nav>
+      <ul>
+        <li><a href="index.php">Em Alta</a></li>
+        <li><a href="?type=mansoes">Mansão</a></li>
+        <li><a href="?type=quartos">Quarto</a></li>
+        <li><a href="?type=fazendas">Fazenda</a></li>
+      </ul>
+    </nav>
+
+  <main>
+      
+    <?php
+        if(isset($data["Mansões"]) && isset($data["Fazendas"]) && isset($data["Quartos"])  ){
+          
+          foreach($data as $iUm => $aCategoria){
+            foreach($aCategoria as $iDois=> $resposta){
+    ?>
+
 <div class="listing-card">
         <a href="indexDetalhes.php?index=<?=$iDois?>&type=<?php
           if($iUm == "Fazendas") echo "fazendas";
